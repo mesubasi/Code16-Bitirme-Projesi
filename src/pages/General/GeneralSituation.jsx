ced0e708840c0a689bf4c0eb772e3ff830c8b4af
Muhammed Emin SUBAŞI <88106043+mesubasi@users.noreply.github.com>
2024-03-06 22:44:14 +0300

    Update GeneralSituation.jsx

--- src/pages/General/GeneralSituation.jsx
+++ src/pages/General/GeneralSituation.jsx
@@ -32,10 +32,6 @@ const GeneralSituation = () => {
     fetchData();
   }, []);
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
     <div className="min-h-screen max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <h2 className="text-2xl font-semibold text-gray-900">Baraj Verileri</h2>
